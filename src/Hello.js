a984f3370492fb780d32be71ad9aeb5489f4fc92
Yo Wook <uk960214@gmail.com>
2020-12-06 23:50:01 +0900

    Initial Commit

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hello.css'
+const Hello = (props) => {
+        return (
+            <div className='f1 tc'>
+                <h1>Hello World</h1>
+                <p>{props.greetings}</p>
+            </div>
+        );
+    }
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greetings={'Hello React Ninja'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
